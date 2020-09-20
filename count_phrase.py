84b9ee28e3781dd5d5684f4aa2f7b8584fbb10d4
Ingroj Shrestha <ing.stha@gmail.com>
2020-09-20 14:11:33 -0500

    script count_phrase added

---
+++ count_phrase.py
@@ -0,0 +1,43 @@
+def get_indices(document, search_term):
+    indices = []
+    for index, token in enumerate(document.split()):
+        if token == search_term:
+            indices.append(index)
+    return indices
+          
+def count_phrase_occurences(document, phrase):
+    '''
+    returns the number of occurrences of a phrase in a document
+
+    param document: a document where a given phrase is searched
+    type document: str
+    
+    param phrase: a phrase to be searched in the document
+    type phrase: str
+    
+    returns: the number of occurrences of the phrase in the document
+    rtype: int
+    
+    '''
+    count= 0 
+    
+    phrase_tokens = phrase.split()
+
+    indices = get_indices(document, phrase_tokens[0]) 
+    
+    for index in indices:
+        #extract new phrase from document of length given phrase length
+        extracted_phrase_tokens = document.split()[index:index+len(phrase_tokens)] 
+        
+        #combine tokens with space to get a phrase
+        extracted_phrase = ' '.join(extracted_phrase_tokens) 
+        
+        #remove delimiters from the end of the extracted phrase
+        delimiters = ['.', '?', ':', ';', '!', ',']
+        for delimiter in delimiters:
+            extracted_phrase = extracted_phrase.rstrip(delimiter)
+        
+        #compare new extracted phrase with the given phrase
+        if phrase == extracted_phrase:
+            count+=1
+    return count
